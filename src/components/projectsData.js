db445b81299351c8d54c1543c3b7285f5034a460
Achintya Shrivastava <85828877+ESHUshri202@users.noreply.github.com>
2025-02-28 20:03:01 +0000

    Project_code_reduced_complexity

---
+++ src/components/projectsData.js
@@ -0,0 +1,104 @@
+// projectsData.js
+
+const projects = [
+    {
+      title: "Medical Recommendation System",
+      details: "Medical Recommendation System is an online platform which can detect the disease using given symptoms. A fully functional machine learning based project with features like disease detection, providing prescription, diet analysis, medicine recommending and workout details.",
+      image: "https://s3-alpha.figma.com/hub/file/5401068013/ac9e79a5-535e-431a-8c2f-3924780c3761-cover.png",
+      techStack: ["Python", "Machine Learning", "Flask"],
+      projectLink: "#"
+    },
+    {
+      title: "Feast of Flavours",
+      details: "Feast of Flavour is a vibrant recipe showcase website that celebrates culinary creativity. It features a wide variety of delicious recipes, from quick meals to gourmet dishes, offering step-by-step instructions to inspire both beginner and experienced cooks to explore diverse flavors.",
+      image: "https://s3-alpha.figma.com/hub/file/6090747941/acb2e473-5d7c-434d-9782-9a2110830fc5-cover.png",
+      techStack: ["HTML", "Tailwind CSS", "React Js"],
+      projectLink: "#"
+    },
+    {
+      title: "Parkison Disease Detection",
+      details: "Parkison Disease Detection is an online platform which can detect the disease using given symptoms. A fully functional machine learning based project with features like disease detection, providing prescription, diet analysis, medicine recommending and workout details.",
+      image: "https://lnpuk.com/wp-content/uploads/2020/10/LNP-Banner-images-1800-x-1125-6-3-1024x640.jpg",
+      techStack: ["Python", "Machine Learning", "Streamlit"],
+      projectLink: "https://eshushri202-parkison-disease-detection-main-0jxv8j.streamlit.app/"
+    },
+    {
+      title: "Company X Template",
+      details: "Company X’s template website, built with React.js and Tailwind CSS, features a modern, responsive design. React enables dynamic content management, while Tailwind CSS ensures a customizable, efficient, and visually appealing user interface, optimized for performance.",
+      image: "https://as2.ftcdn.net/v2/jpg/05/44/23/79/1000_F_544237902_yi7ivAoP3PuyCj2LD5Mb4ZrKXvUuHsiT.jpg",
+      techStack: ["HTML", "Tailwind CSS", "React Js"],
+      projectLink: "https://company-x-demo.vercel.app/"
+    },
+    {
+      title: "Stock Price Prediction Model",
+      details: "A stock price prediction model leverages historical stock data, technical indicators, and sometimes market sentiment to estimate future price movements. It often employs machine learning techniques, including LSTM, ARIMA, and regression, for time-series analysis and forecasting.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMVRMXfIOBXt8z5GGv10QPw6r6vzyhCTyjXA&s",
+      techStack: ["Python", "Machine Learning", "Streamlit"],
+      projectLink: "https://eshushri202-stock-price-prediction-model-app-4emffq.streamlit.app/"
+    },
+    {
+      title: "News App",
+      details: "Built with JavaScript, this news app delivers timely updates and personalized content to users across web and mobile. It uses JavaScript for dynamic data rendering, seamless navigation, and real-time notifications, creating an engaging and responsive news-reading experience.",
+      image: "https://colorlib.com/wp/wp-content/uploads/sites/2/9_best_news_website_design.jpg",
+      techStack: ["HTML", "CSS", "React Js"],
+      projectLink: "https://news-app-rho-jet.vercel.app/"
+    },
+    {
+      title: "Mamba AI",
+      details: "Mamba AI, a React.js-based clone of Gemini, replicates Gemini’s conversational AI features. Utilizing React’s component-driven architecture, it delivers real-time interactions, smooth UI updates, and AI responses, offering users a seamless, dynamic chat experience similar to Gemini.",
+      image: "https://i.ytimg.com/vi/iV0ZIczwX3I/maxresdefault.jpg",
+      techStack: ["HTML", "CSS", "React Js"],
+      projectLink: "https://mamba-ai.netlify.app/"
+    },
+    {
+      title: "Smart Attendance System",
+      details: "A smart attendance system using facial recognition automates attendance tracking by identifying individuals through their facial features. It enhances accuracy, reduces fraud, and streamlines processes by recording attendance in real time, offering a seamless and secure alternative to traditional methods.",
+      image: "https://www.lystloc.com/blog/wp-content/uploads/2022/11/ezgif.com-gif-maker-6.webp",
+      techStack: ["Python", "OpenCV"],
+      projectLink: "#"
+    },
+    {
+      title: "Netflix Clone",
+      details: "This Netflix clone, created with React.js, provides a responsive, interactive streaming experience. Using React components, it supports features like movie browsing, user authentication, and video playback. The app dynamically fetches data, offering a user-friendly interface similar to Netflix.",
+      image: "https://cdn3.f-cdn.com//files/download/128385152/netflix1.jpg?width=780&height=401&fit=crop",
+      techStack: ["HTML", "CSS", "React Js"],
+      projectLink: "https://netflix-clone-beta-rust.vercel.app/"
+    },
+    {
+      title: "Corona Survival Dashboard",
+      details: "The Corona Survival Dashboard, created using Power BI, visualizes key COVID-19 data like cases, recoveries, and vaccination rates. It provides real-time insights through interactive charts, helping users track trends and make informed decisions based on up-to-date information.",
+      image: "https://aaqr.org/images/article_images/2020/10/0314_fig1.png",
+      techStack: ["Power BI"],
+      projectLink: "#"
+    },
+    {
+      title: "Indian Oil Corporation Report Analysis",
+      details: "A comprehensive analysis of Indian Oil Corporations share price trends over time. It visualizes key metrics like open, close, high, and low prices, alongside adjusted close prices and trading volumes. It helps investors track performance and market trends.",
+      image: "https://media.licdn.com/dms/image/v2/D5622AQHwjhHWcfLchw/feedshare-shrink_800/feedshare-shrink_800/0/1733425518066?e=1743638400&v=beta&t=ZxU7zvvtaE_fCa9-9VVrNt9jXwTAodtde-NkDBqc5cs",
+      techStack: ["Power BI"],
+      projectLink: "#"
+    },
+    {
+      title: "Project Management Report Dashboard",
+      details: "A comprehensive Power BI dashboard designed to monitor and analyze project performance across multiple dimensions. This dashboard provides actionable insights into key project metrics, empowering data-driven decision-making.",
+      image: "https://media.licdn.com/dms/image/v2/D5622AQHwpZ_f4ekD6g/feedshare-shrink_800/feedshare-shrink_800/0/1733486566460?e=1743638400&v=beta&t=ll88GojZbns8cDgXE69qh-S0oXfJpFWsBblo5CgcRtw",
+      techStack: ["Power BI"],
+      projectLink: "#"
+    },
+    {
+      title: "North Sea Export Sales Report (2010 - 2013) Dashboard",
+      details: "The North Sea Export Sales Report (2010–2013) reveals $9.30M in sales, $8.97M in costs, and $120.61K in losses. Sales varied across regions, with furniture having the highest loss. Technology led in sales after discounts.",
+      image: "https://media.licdn.com/dms/image/v2/D5622AQGtCC70ldBo2g/feedshare-shrink_800/B56ZT2BLLtGUAg-/0/1739294292873?e=1743638400&v=beta&t=w_7ZNAFMuzpVWwWGADj3Pso_mNtOCZgv3O_qz3T_7Fw",
+      techStack: ["Power BI"],
+      projectLink: "#"
+    },
+    {
+      title: "Dog & Cat Classification",
+      details: "Dog & Cat Classification using TensorFlow leverages deep learning models, typically Convolutional Neural Networks (CNNs), to accurately classify images as either dogs or cats. The model is trained on labeled image datasets to recognize distinctive features of each animal.",
+      image: "https://cdn.analyticsvidhya.com/wp-content/uploads/2021/06/how-to-do-Cat-and-Dog-Classification-using-CNN_-How-to-do-Cat-and-Dog-Classification-using-CNN_-01-scaled.jpg",
+      techStack: ["Python", "TensorFlow"],
+      projectLink: "#"
+    },
+  ];
+  
+  export default projects;
