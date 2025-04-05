b62c686d5f3767e6f7c13fc31c1f4e1192b666bf
Achintya Shrivastava <85828877+ESHUshri202@users.noreply.github.com>
2025-04-05 17:38:06 +0000

    added_loading

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
+import Loading from './components/Loading'
 const Layout = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay (e.g., fetching data)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the time as needed
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <>
         <Navbar/>
